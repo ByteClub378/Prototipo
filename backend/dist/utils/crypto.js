ae86cfbe4277d48e30c6e41ce7b30e0bb78e58ff
KauaAmaroPires <kauapiresamaro@gmail.com>
2026-09-03 14:04:36 -0300

    DELETE arquivo duplicado

--- backend/dist/utils/crypto.js
+++
@@ -1,5 +0,0 @@
-import { createHash, randomBytes, randomUUID } from "node:crypto";
-export const newId = () => randomUUID();
-export const newOpaqueToken = () => randomBytes(32).toString("base64url");
-export const hashToken = (token) => createHash("sha256").update(token).digest();
-//# sourceMappingURL=crypto.js.map
